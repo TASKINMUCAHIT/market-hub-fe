3caf142c65836ed27e6646956556ce3fc917cee3
TASKINMUCAHIT <51125872+TASKINMUCAHIT@users.noreply.github.com>
2025-12-06 20:25:40 +0300

    Fix defaultauth typo: use 'fakebackend'

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  defaultauth: 'fackbackend',
+  defaultauth: 'fakebackend',
   firebaseConfig: {
     apiKey: '',
     authDomain: '',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  defaultauth: 'fackbackend',
+  defaultauth: 'fakebackend',
   firebaseConfig: {
     apiKey: '',
     authDomain: '',
